1cb50df84a1016cd820fc4b4963a05f9405b9419
jimm <Jim@two-bits.net>
2023-05-16 06:53:01 -0700

    initial commit

---
+++ ContainsKeySample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ContainsKeySample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ ContainsKeySample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ContainsKeySample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
